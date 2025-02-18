9c64de6a27c0d67f6ccb53cd0ea9d1746363139b
Nekonic <kybplay1234@gmail.com>
2025-02-19 00:44:08 +0900

    update

--- 독하게_시작하는_JAVA/part2/04-Shape/src/Shape.java
+++ 독하게_시작하는_JAVA/part2/04-Shape/src/Shape.java
@@ -1,32 +1,28 @@
 public class Shape {
-    protected int[][] px = new int[5][5];
     public void render(){
-        for(int i=0; i<5; i++){
-            for(int j=0; j<5; j++){
-                if (px[i][j] == 1)System.out.print("*\t");
-                else System.out.print(" \t");
+    }
+}
+
+class Rectangle extends Shape{
+    @Override
+    public void render(){
+        for(int i=0; i<5; ++i){
+            for(int j=0; j<5; ++j){
+                System.out.print("* ");
             }
             System.out.println();
         }
     }
 }
 
-class Rectangle extends Shape{
-    Rectangle(){
-        for(int i=0; i<5; i++){
-            for(int j=0; j<5; j++){
-                px[i][j]=1;
-            }
-        }
-    }
-}
-
 class Triangle extends Shape{
-    Triangle(){
-        for(int i=0; i<5; i++){
-            for(int j=0; j<=i; j++){
-                px[i][j]=1;
+    @Override
+    public void render(){
+        for(int i=0; i<5; ++i){
+            for(int j=0; j<=i; ++j){
+                System.out.print("* ");
             }
+            System.out.println();
         }
     }
 }
